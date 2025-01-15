0bf68b14748979d8ddd7356c6e679619c965bfe9
Jes <suarezliceajesus@gmail.com>
2025-01-15 13:05:59 -0600

    Actualice el script de gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,67 +1,47 @@
-const { src, dest, watch , parallel } = require('gulp');
+const { src, dest, watch, parallel } = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const autoprefixer = require('autoprefixer');
-const postcss    = require('gulp-postcss')
-const sourcemaps = require('gulp-sourcemaps')
+const postcss = require('gulp-postcss');
+const sourcemaps = require('gulp-sourcemaps');
 const cssnano = require('cssnano');
 const concat = require('gulp-concat');
 const terser = require('gulp-terser-js');
 const rename = require('gulp-rename');
-const imagemin = require('gulp-imagemin');
-const notify = require('gulp-notify');
-const cache = require('gulp-cache');
-const webp = require('gulp-webp');
 
 const paths = {
     scss: 'src/scss/**/*.scss',
     js: 'src/js/**/*.js',
-    imagenes: 'src/img/**/*'
-}
+};
 
-// css es una función que se puede llamar automaticamente
+// Tarea para procesar SASS
 function css() {
     return src(paths.scss)
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(postcss([autoprefixer(), cssnano()]))
-        // .pipe(postcss([autoprefixer()]))
         .pipe(sourcemaps.write('.'))
-        .pipe( dest('./build/css') );
+        .pipe(dest('./build/css'));
 }
 
-
+// Tarea para procesar JavaScript
 function javascript() {
     return src(paths.js)
-      .pipe(sourcemaps.init())
-      .pipe(concat('bundle.js')) // final output file name
-      .pipe(terser())
-      .pipe(sourcemaps.write('.'))
-      .pipe(rename({ suffix: '.min' }))
-      .pipe(dest('./build/js'))
+        .pipe(sourcemaps.init())
+        .pipe(concat('bundle.js')) // Nombre del archivo de salida
+        .pipe(terser())
+        .pipe(sourcemaps.write('.'))
+        .pipe(rename({ suffix: '.min' }))
+        .pipe(dest('./build/js'));
 }
 
-function imagenes() {
-    return src(paths.imagenes)
-        .pipe(cache(imagemin({ optimizationLevel: 3})))
-        .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
-}
-
-function versionWebp() {
-    return src(paths.imagenes)
-        .pipe( webp() )
-        .pipe(dest('build/img'))
-        .pipe(notify({ message: 'Imagen Completada'}));
-}
-
-
+// Tarea para observar cambios en archivos SCSS y JS
 function watchFiles() {
-    watch( paths.scss, css );
-    watch( paths.js, javascript );
-    watch( paths.imagenes, imagenes );
-    watch( paths.imagenes, versionWebp );
+    watch(paths.scss, css);
+    watch(paths.js, javascript);
 }
-  
+
+// Exportar las tareas
 exports.css = css;
+exports.javascript = javascript;
 exports.watchFiles = watchFiles;
-exports.default = parallel(css, javascript, imagenes, versionWebp, watchFiles); 
+exports.default = parallel(css, javascript, watchFiles);
